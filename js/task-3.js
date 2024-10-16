8410a3a968fdf56107351be438f208ddff7926ab
Serhii Kuzmenko <sergey.kuzmenko2110@gmail.com>
2024-10-16 12:50:33 +0300

    .

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+  const content1 = parseFloat(content);
+  const padding1 = parseFloat(padding);
+  const border1 = parseFloat(border);
+  const totalPixel = content1 + padding1 * 2 + border1 * 2;
+  return totalPixel;
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
