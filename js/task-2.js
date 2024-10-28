cb36e7a4d43466904f4a8f0a35c0557efcabd4cb
Serhii Kuzmenko <sergey.kuzmenko2110@gmail.com>
2024-10-28 12:29:28 +0300

    .

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,7 @@
 function getShippingMessage(country, price, deliveryFee) {
   const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+  const message = `Shipping to ${country} will cost ${totalPrice} credits`;
+  return message;
 }
 
 console.log(getShippingMessage("Australia", 120, 50));
--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,13 @@
 function getElementWidth(content, padding, border) {
-return parseFloat(content);
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
+    return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
 
-//const content1 = parseFloat(content);
-//const padding1 = parseFloat(padding);
-//const border1 = parseFloat(border);
-//const totalPixel = content1 + padding1 * 2 + border1 * 2;
-//return totalPixel;
+
