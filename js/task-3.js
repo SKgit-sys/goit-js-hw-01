d203e46b15f13320f0fd6c20c56252865965159e
Serhii Kuzmenko <sergey.kuzmenko2110@gmail.com>
2024-11-02 18:19:24 +0300

    .

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,9 @@
 function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content)
-    return contentWidth;
-    // const contentWidth = parseFloat(content);
-    // const paddingWidth = parseFloat(padding);
-    // const borderWidth = parseFloat(border);
-    // const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
-    // return totalWidth;
-
-
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
+    return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
